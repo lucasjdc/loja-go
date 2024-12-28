5131e5f25a094a293ad3dbbedf29e1fbedc57163
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-12-27 21:06:39 -0300

    Buscando dados da página

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"html/template"
+	"log"
+	"strconv"
 	"net/http"
 	"loja/models"
 )
@@ -18,4 +20,27 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func New(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+		
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+		
+		quatidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+		
+		models.CriaNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+		
+	}
+	http.Redirect(w, r, "/", 301)
 }
--- models/produtos.go
+++ models/produtos.go
@@ -40,3 +40,7 @@ func BuscaTodosOsProdutos() []Produto {
 	defer db.Close()
 	return  produtos
 }
+
+func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+}
